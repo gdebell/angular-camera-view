15de57ef299aae7ed3356340a2f785d0ca9d9e5a
Gina <gdebell@gmail.com>
2016-10-12 15:46:09 -0600

    init

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 (function() {
   'use strict';
-
+  var app = angular.module('cameraApp', []);
   $('select').material_select();
+  app.filter('makeStars', function() {
+    return function (input) {
+      console.log(input);
+      return Array(input).join("*");
+    };
+  });
+
+app.controller('ManageCart', ManageCart);
+
+function ManageCart() {
+  this.camera = {
+    name: '',
+    price: 0
+  };
+}
 
 })();
