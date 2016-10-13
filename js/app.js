569a55b765b3662897f3395dd18db2b01dade61e
Gina <gdebell@gmail.com>
2016-10-12 19:53:34 -0600

    camera name and price is added to cart

--- js/app.js
+++ js/app.js
@@ -4,18 +4,38 @@
   $('select').material_select();
   app.filter('makeStars', function() {
     return function (input) {
-      console.log(input);
+      //console.log(input);
       return Array(input).join("*");
     };
   });
+  app.controller('CartController', CartController);
+  app.controller('ProductController', ProductController);
 
-app.controller('ManageCart', ManageCart);
+  function CartController ($rootScope) {
+    this.items = [];
+    this.item = {
+      name:'',
+      price: 0
+    }
 
-function ManageCart() {
-  this.camera = {
-    name: '',
-    price: 0
-  };
-}
+    this.addItem = (camera) => {
+      const newCamera = {
+        id: camera.id,
+        name: camera.name,
+        cost: camera.price
+      }
+      //console.log('clicked addItem btn!!');
+      console.log(newCamera);
+      const name = newCamera.name;
+      const cost = newCamera.cost;
+      const id = newCamera.id;
+      this.items.push(newCamera)
+      $rootScope.shoppingCart = `${name}    ${cost}`
+    }
+  }
+
+  function ProductController ($rootScope) {
+    $rootScope.shoppingCart = ''
+  }
 
 })();
